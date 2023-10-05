726865bf34c0cec7bf83fcf2c4f90ad7fdea005c
Kimutai <jankimsingoei@gmail.com>
2023-10-05 11:16:14 +0300

    Modify CLI commands

--- lib/models.py
+++ lib/models.py
@@ -1,43 +1,74 @@
 #!usr/bin/ env python3
-from sqlalchemy import create_engine,Column,Integer,String,Float
-from sqlalchemy.orm import declarative_base,sessionmaker
+from datetime import datetime
+from sqlalchemy import create_engine,Column,Integer,String,Float,DateTime,Table,ForeignKey
+from sqlalchemy.orm import declarative_base,sessionmaker,relationship
 from prettytable import PrettyTable
 
 Base = declarative_base()
 
+class_staff = Table(
+    "class_staff",
+    Base.metadata,
+    Column('class_id', Integer(), ForeignKey('class.id')),
+    Column("staff_id", Integer(), ForeignKey("staff.id")),
+    extend_existing=True
+)
+
 class Student(Base):
     __tablename__ = 'student_detail'
 
-    id = Column(Integer(), primary_key = True)
+    id = Column(Integer(), primary_key=True)
     first_name = Column(String())
     last_name = Column(String())
     gender = Column(String())
-    email = Column(String(),unique=True)
-    phone_number= Column(Integer())
+    email = Column(String(), unique=True)
+    phone_number = Column(Integer())
+    class_id = Column(Integer, ForeignKey('class.id'))
+    class_data = relationship("Class", back_populates="students")
+    staff_id = Column(Integer, ForeignKey('staff.id'))
+    staff = relationship("Staff", back_populates="students")
+    enrolled_on = Column(DateTime,default=datetime.now())
 
-class Course(Base):
-    __tablename__ = 'courses'
+class Class(Base):
+    __tablename__ = 'class'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    course = Column(String())
+    students = relationship("Student", back_populates="class_data")
+    staffs = relationship("Staff", secondary=class_staff, back_populates="classes")
 
-    id = Column(Integer(), primary_key = True)
+class Staff(Base):
+    __tablename__ = 'staff'
+
+    id = Column(Integer(), primary_key=True)
     name = Column(String())
-    
+    role = Column(String())
+    contact = Column(Integer())
+    classes = relationship('Class', secondary=class_staff, back_populates="staffs")
+    students = relationship('Student', back_populates="staff")
+
+
 if __name__ == "__main__":
     engine = create_engine('sqlite:///school.db')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind = engine)
     session =Session()
-
-    print("---------SCHOOL RECORDS MANAGEMENT SYSTEM---------")
+    #menu items
     menu = '''
-1: CREATE STUDENT RECORD
-2: VIEW STUDENT RECORDS
-3: UPDATE STUDENT RECORDS
-4: DELETE STUDENT RECORDS
-5: ADD NEW COURSE
-6: VIEW COURSES
-7: UPDATE COURSE
-8: DELETE COURSE
-9: EXIT
+========================================================
+    SCHOOL RECORDS MANAGEMENT SYSTEM
+========================================================
+
+    1: CREATE STUDENT RECORD                          
+    2: VIEW STUDENT RECORDS
+    3: UPDATE STUDENT RECORDS
+    4: DELETE STUDENT RECORDS
+    5: ADD NEW CLASS
+    6: VIEW CLASSES
+    7: ADD STAFF
+    8: VIEW STAFF
+    9: EXIT
+========================================================
 '''
 
 # Define the menu loop
@@ -53,17 +84,31 @@ while True:
         new_student.gender = input('Enter Gender: ')
         new_student.email = input('Enter Email: ')
         new_student.phone_number = input('Enter Phone Number: ')
-        session.add(new_student)
-        session.commit()
-        print('Student details saved successfully')
+        new_student.class_id = input('Enter CLASS ID: ')
+        new_student.staff_id = input('Enter STAFF ID: ')
+        new_student.enrolled_on = datetime.now()
+
+        class_exists= session.query(Class).filter_by(id = new_student.class_id).first()
+        staff_exists= session.query(Staff).filter_by(id = new_student.staff_id).first()
+
+        if staff_exists and class_exists:
+            session.add(new_student)
+            session.commit()
+            print('Student details saved successfully')   
+        else:
+            print('Invalid CLASS ID or STAFF ID entered')
+
     elif user_choice == "2":
         # View student records
         students = session.query(Student).all()
         # Create a table object
         table = PrettyTable()
-        table.field_names = ['ID', 'First Name', 'Last Name', 'Gender', 'Email', 'Phone Number']
+        table.field_names = ['ID','Enrolled_On', 'First Name', 'Last Name', 'Gender', 'Email', 'Phone Number','Class ID','Staff Id']
+       
         for student in students:
-            table.add_row([student.id, student.first_name, student.last_name, student.gender, student.email, student.phone_number])
+            date_object = student.enrolled_on
+            date_string = date_object.strftime('%Y-%m-%d')
+            table.add_row([student.id,date_string, student.first_name, student.last_name, student.gender, student.email, student.phone_number,student.class_id,student.staff_id])
         print(table)
     elif user_choice == "3":
         # Update student records
@@ -76,8 +121,19 @@ while True:
             student.gender = input('Enter Gender: ')
             student.email = input('Enter Email: ')
             student.phone_number = input('Enter Phone Number: ')
-            session.commit()
-            print(f'Student with ID {student_id} updated successfully')
+            student.class_id = input('Enter CLASS ID: ')
+            student.staff_id = input('Enter STAFF ID: ')
+            student.enrolled_on = datetime.now()
+
+            class_exists= session.query(Class).filter_by(id = student.class_id).first()
+            staff_exists= session.query(Staff).filter_by(id = student.staff_id).first()
+            if class_exists and staff_exists:
+                print('Invalid CLASS ID or STAFF ID entered')
+            else:
+                session.commit()
+                print(f'Student with ID {student_id} enrolled successfully')
+
+            
     elif user_choice == "4":
         # Delete student records
         print("------------------------------------------------")
@@ -91,43 +147,48 @@ while True:
         else:
             print(f'Student with ID {student_id} not found')
     elif user_choice == "5":
-        # Add new course
+        # Add new class
         print("------------------------------------------------")
-        new_course = Course(name=input('Enter New Course: '))
-        session.add(new_course)
-        session.commit()
-        print('Course added successfully')
+        new_class = Class()
+        new_class.name = input('Enter class name: ')
+        new_class.course = input('Enter Course Name: ')
+
+        name_exists = session.query(Class).filter_by(name = new_class.name).first()
+        if name_exists:
+            print("Class Aleady exists")
+            
+        else:
+            session.add(new_class)
+            session.commit()
+            print("Class Added Successfully")
     elif user_choice == "6":
-        #view courses
-        course_data = session.query(Course).all()
+        #view class
+        class_data = session.query(Class).all()
         table = PrettyTable()
-        table.field_names = ["COURSE ID" , "COURSE NAME"]
-        for course in course_data:
-            table.add_row([course.id,course.name])
+        table.field_names = ["CLASS ID" , "CLASS NAME","COURSE"]
+        for class_instance in class_data:
+            table.add_row([class_instance.id,class_instance.name,class_instance.course])
        
         print(table)
     elif user_choice == "7":
-        #Update Course
-        print("------------------------------------------------")
-        course_id = input('Enter the ID of the course to update: ')
-        course = session.query(Course).filter_by(id=course_id).first()
-        if course:
-            course.name = input('Enter the new name of the course: ')
-            session.commit()
-            print(f'Course with ID {course_id} updated successfully')
+       #add staff
+       staff_record = Staff()
+       staff_record.name = input("ENTER Name: ")
+       staff_record.role = input('Enter Role: ')
+       staff_record.contact = input('Enter Phone Number: ')
+
+       session.add(staff_record)
+       session.commit()
 
     elif user_choice == "8":
-        #delete course
-        print("------------------------------------------------")
-        course_id = input('Enter the ID of the course to delete: ')
-        course = session.query(Course).filter_by(id=course_id).first()
-        if course:
-            # Delete the course
-            session.delete(course)
-            session.commit()
-            print(f'Course with ID {course_id} deleted successfully')
-        else:
-            print(f'Course with ID {course_id} not found')
+        #view staff
+        staff_data = session.query(Staff).all()
+        table = PrettyTable()
+        table.field_names = [" STAFF ID" , "NAME", "ROLE"," PHONE NUMBER"]
+
+        for staff in staff_data:
+            table.add_row([staff.id,staff.name,staff.role,staff.contact])
+        print(table)
     elif user_choice == "9":
         #exiting the program
         print('Exiting the program...')
